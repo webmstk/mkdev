92ea0e90530893ef9bceead0a3871351ca8190c7
webmstk <webmstk@mail.ru>
2015-12-07 23:54:12 +0300

    lesson 6

--- movies.rb
+++ movies.rb
@@ -1,8 +1,8 @@
 require 'csv'
 require 'date'
-require 'ostruct'
 require './movie'
 require './movie_list'
+require './my_movie_list'
 
 def print_header(header)
   puts "\n***** #{header} *****\n\n"
@@ -36,33 +36,67 @@ def print_by_director(films, header = nil)
   print_header(header) if header
 
   print films
-    .group_by(&:director)
-    .sort
-    .map { |director, films| "#{director}: #{films.count}" }
-    .join(', ')
+            .group_by(&:director)
+            .sort
+            .map { |director, films| "#{director}: #{films.count}" }
+            .join(', ')
 end
-
 
 def print_by_months(films, header = nil)
   print_header(header) if header
 
   films
-    .map(&:release_date)
-    .reject { |date| date.count('-') < 1 }
-    .map { |date| Date.strptime(date, '%Y-%m') }
-    .group_by(&:month)
-    .sort
-    .each { |month, dates| puts "#{Date::MONTHNAMES[month.to_i]}: #{dates.count} film(s)" }
+      .map(&:release_date)
+      .reject { |date| date.count('-') < 1 }
+      .map { |date| Date.strptime(date, '%Y-%m') }
+      .group_by(&:month)
+      .sort
+      .each { |month, dates| puts "#{Date::MONTHNAMES[month.to_i]}: #{dates.count} film(s)" }
+end
+
+def print_short(films)
+  films.each do |film|
+    puts "#{film.name} | #{film.class} | #{film.year} | #{film.rating_to_stars}"
+  end
+end
+
+def print_rec(films, header = nil)
+  print_header(header) if header
+
+  films.each do |film|
+    puts '-' * 100
+    puts film.description
+    puts '-' * 100
+  end
 end
 
 
 # EXECUTE
 
-list = MovieList.new
-print_films(list.find('Time'), 'Search "Time"')
+# list = MovieList.new
+# print_films(list.find('Time'), 'Search "Time"')
 # print_films(list.long_films, 'The Longest Films')
 # print_films(list.by_genre('Comedy'), 'Comedy list')
 # print_directors(list.directors, 'Directors list')
 # print_count_films(list.except_country('USA'), 'Films was made not in USA')
 # print_by_director(list.films, 'Films grouped by director')
 # print_by_months(list.films, 'Films grouped by month')
+
+
+mylist = MyMovieList.new
+
+films_seen = [
+    {name: 'The Good, the Bad and the Ugly', myrating: 9},
+    {name: 'Fight Club', myrating: 10},
+    {name: 'Goodfellas', myrating: 5},
+    {name: 'Bicycle Thieves', myrating: 1},
+    {name: 'Full Metal Jacket', myrating: 2},
+    {name: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb', myrating: 3},
+]
+
+mylist.watch_film('The Godfather', 7, Date.new(2004, 10))
+mylist.watch_film('The Dark Knight', 8, Date.new(2003, 3))
+mylist.watch_films(films_seen)
+
+print_rec(mylist.recommend_films, 'ПОСМОТРИТЕ СЕГОДНЯ ВЕЧЕРОМ!')
+print_rec(mylist.recommend_films_seen, 'ФИЛЬМЫ, КОТОРЫЕ СТОИТ ПОСМОТРЕТЬ НЕ РАЗ!')
