a87b6f82c129b5ccf5075e67ec93ed64e7749a71
webmstk <webmstk@mail.ru>
2015-12-07 17:35:37 +0300

    lesson 5 - remarks

--- movie.rb
+++ movie.rb
@@ -34,4 +34,45 @@ class Movie
   def country?(c)
     country == c
   end
+
+
+  def has_genre?(genre)
+    self.genre.include? genre
+  end
+
+
+  def get_rating
+    '*' * rating[-1].to_i
+  end
+
+
+  def get_stars
+    stars.split(',').join(', ')
+  end
+
+
+  def get_genre
+    genre.split(',').join(', ')
+  end
+
+
+  def format_release_date
+    delimiters = release_date.count('-')
+
+    date = case delimiters
+      when 2
+        Date.strptime(release_date, '%Y-%m-%d')
+      when 1
+        Date.strptime(release_date, '%Y-%m')
+      when 0
+        Date.strptime(release_date, '%Y')
+    end
+    
+    date.strftime('%m %B %Y')
+  end
+
+
+  def country?(country)
+    self.country == country
+  end
 end
