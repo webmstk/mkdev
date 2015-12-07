f43f518a6d80a11168bddc00d7fd4a1f7270d0b8
webmstk <webmstk@mail.ru>
2015-12-07 23:54:12 +0300

    lesson 6

--- movie.rb
+++ movie.rb
@@ -1,8 +1,10 @@
 class Movie
-  attr_accessor :url, :name, :year, :country, :release_date, :genre, :duration, :rating, :director, :stars
+  PREFERENCE = 50
+  attr_accessor :url, :name, :year, :country, :release_date, :genre, :duration, :rating, :director, :stars, :myrating, :date_seen
 
-  def initialize args
-    args.each do |k,v|
+  def initialize(owner, args)
+    @owner = owner
+    args.each do |k, v|
       instance_variable_set("@#{k}", v) unless v.nil?
     end
 
@@ -10,14 +12,19 @@ class Movie
     @genre = @genre.split(',')
 
     @sort_date = case release_date.count('-')
-      when 2
-        Date.strptime(release_date, '%Y-%m-%d')
-      when 1
-        Date.strptime(release_date, '%Y-%m')
-      when 0
-        Date.strptime(release_date, '%Y')
-    end
-      .strftime('%m %B %Y')
+                   when 2
+                     Date.strptime(release_date, '%Y-%m-%d')
+                   when 1
+                     Date.strptime(release_date, '%Y-%m')
+                   when 0
+                     Date.strptime(release_date, '%Y')
+                 end
+                     .strftime('%m %B %Y')
+  end
+
+
+  def watched?
+    date_seen.nil?
   end
 
 
@@ -36,17 +43,42 @@ class Movie
   end
 
 
-  def has_genre?(genre)
-    self.genre.include? genre
+  def description
+    name
   end
+end
 
+class AncientMovie < Movie
+  PREFERENCE = 20
 
-  def get_rating
-    '*' * rating[-1].to_i
+  def description
+    "#{name} - старый фильм (#{year} год)"
   end
+end
 
+class ClassicMovie < Movie
+  PREFERENCE = 40
 
-  def get_stars
-    stars.split(',').join(', ')
+  def description
+    director_films = @owner
+                         .by_director(director)
+                         .map(&:name)
+    "#{name} - классика, режиссер #{director} (#{director_films.join(', ')})"
+  end
+end
+
+class ModernMovie < Movie
+  PREFERENCE = 60
+
+  def description
+    "#{name} - современное кино (в ролях: #{stars.join('; ')})"
+  end
+end
+
+class NewMovie < Movie
+  PREFERENCE = 100
+
+  def description
+    "#{name} - НОВЫЙ! (жанр: #{genre.join(', ')})"
   end
 end
