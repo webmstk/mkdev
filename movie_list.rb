cf1c754cd2073b7eeb2474f51032749e31536b69
webmstk <webmstk@mail.ru>
2015-12-09 21:15:27 +0300

    lesson 7

--- movie_list.rb
+++ movie_list.rb
@@ -1,18 +1,20 @@
 class MovieList
   FIELDS = [:url, :name, :year, :country, :release_date, :genre, :duration, :rating, :director, :stars]
 
-  attr_reader :films
+  attr_reader :movies
 
   def initialize(filename = 'movies.txt')
     read_file(filename)
+    @sort_algo = {}
+    @filter = {}
   end
 
 
   def read_file filename
-    @films = []
+    @movies = []
 
     begin
-      @films = CSV.read(filename, col_sep: '|', headers: FIELDS).map { |line| Movie.new(self, line.to_hash) }
+      @movies = CSV.read(filename, col_sep: '|', headers: FIELDS).map { |line| Movie.new(self, line.to_hash) }
     rescue
       puts "File not found: #{filename}"
     end
@@ -20,34 +22,34 @@ class MovieList
 
 
   def find string
-    @films.select { |film| film.name.downcase.include? string.downcase }
+    @movies.select { |movie| movie.name.downcase.include? string.downcase }
   end
 
 
   def find_by_name name
-    @films.select { |film| return film if film.name.downcase.include? name.downcase }
+    @movies.select { |movie| return movie if movie.name.downcase.include? name.downcase }
   end
 
 
-  def long_films quantity = 5
-    @films
-        .sort_by { |film| film.duration.to_i }
+  def long_movies quantity = 5
+    @movies
+        .sort_by { |movie| movie.duration.to_i }
         .reverse
         .first(quantity)
   end
 
 
   def by_genre genre
-    @films
-        .select { |film| film.has_genre? genre }
+    @movies
+        .select { |movie| movie.has_genre? genre }
         .sort_by(&:release_date)
   end
 
 
   def directors
-    @films
-        .map do |film|
-      ar = film.director.split
+    @movies
+        .map do |movie|
+      ar = movie.director.split
       name = ar.shift
       ar.push(name).join(' ')
     end.uniq.sort
@@ -55,11 +57,63 @@ class MovieList
 
 
   def except_country country
-    @films.reject { |film| film.country?(country) }
+    @movies.reject { |movie| movie.country?(country) }
   end
 
 
   def by_director director
-    @films.select { |film| film.director == director }
+    @movies.select { |movie| movie.director == director }
+  end
+
+
+  def print &block
+    block ||= lambda { |movie| "#{movie.title}" }
+
+    # @movies.each do |movie|
+    #   puts block.call(movie)
+    # end
+    puts @movies.map(&block).join("\n")
+  end
+
+
+  def add_sort_algo(algo, &block)
+    @sort_algo[algo] = block
+  end
+
+
+  def sorted_by(algo = nil, &block)
+    # if @sort_algo.has_key? algo
+    #   @movies.sort_by { |movie| @sort_algo[algo].call(movie) }
+    # else
+    #   @movies.sort_by(&block)
+    # end
+
+    sorter = case
+               when algo
+                 @sort_algo[algo] or raise("Unknown algo: #{algo}")
+               when block
+                 block
+               else
+                 raise('How can I sort this shit???')
+             end
+
+    @movies.sort_by(&sorter)
+  end
+
+
+  def add_filter(filter, &block)
+    @filter[filter] = block
+  end
+
+
+  def filter filters
+    filters.reduce(@movies) do |memo, (name, value)|
+      # filter = @filter[name]
+      # raise 'wrong filter' unless filter
+
+      filter = @filter[name] or raise("Unknown filter: #{name}")
+
+      memo.select { |movie| filter.call(movie, *value) }
+    end
   end
 end
--- movies.rb
+++ movies.rb
@@ -8,16 +8,16 @@ def print_header(header)
   puts "\n***** #{header} *****\n\n"
 end
 
-def print_films(films, header = nil)
+def print_movies(movies, header = nil)
   print_header(header) if header
 
-  films.each do |film|
-    puts "Film: \"#{film.name}\", #{film.country}, #{film.year}, (#{film.genre.join(', ')})"
-    puts "Rating IMDB: #{film.rating_to_stars}"
-    puts "Director: #{film.director}"
-    puts "Stars: #{film.stars.join(', ')}"
-    puts "Duration: #{film.duration}"
-    puts "Release date: #{film.release_date}"
+  movies.each do |movie|
+    puts "Movie: \"#{movie.name}\", #{movie.country}, #{movie.year}, (#{movie.genre.join(', ')})"
+    puts "Rating IMDB: #{movie.rating_to_stars}"
+    puts "Director: #{movie.director}"
+    puts "Stars: #{movie.stars.join(', ')}"
+    puts "Duration: #{movie.duration}"
+    puts "Release date: #{movie.release_date}"
     puts "\n"
   end
 end
@@ -27,45 +27,39 @@ def print_directors(directors, header = nil)
   print directors.join(', ')
 end
 
-def print_count_films(films, header = nil)
+def print_count_movies(movies, header = nil)
   print_header(header) if header
-  puts "Found: #{films.count} film(s)"
+  puts "Found: #{movies.count} movie(s)"
 end
 
-def print_by_director(films, header = nil)
+def print_by_director(movies, header = nil)
   print_header(header) if header
 
-  print films
+  print movies
             .group_by(&:director)
             .sort
-            .map { |director, films| "#{director}: #{films.count}" }
+            .map { |director, movies| "#{director}: #{movies.count}" }
             .join(', ')
 end
 
-def print_by_months(films, header = nil)
+def print_by_months(movies, header = nil)
   print_header(header) if header
 
-  films
+  movies
       .map(&:release_date)
       .reject { |date| date.count('-') < 1 }
       .map { |date| Date.strptime(date, '%Y-%m') }
       .group_by(&:month)
       .sort
-      .each { |month, dates| puts "#{Date::MONTHNAMES[month.to_i]}: #{dates.count} film(s)" }
+      .each { |month, dates| puts "#{Date::MONTHNAMES[month.to_i]}: #{dates.count} movie(s)" }
 end
 
-def print_short(films)
-  films.each do |film|
-    puts "#{film.name} | #{film.class} | #{film.year} | #{film.rating_to_stars}"
-  end
-end
-
-def print_rec(films, header = nil)
+def print_rec(movies, header = nil)
   print_header(header) if header
 
-  films.each do |film|
+  movies.each do |movie|
     puts '-' * 100
-    puts film.description
+    puts movie.description
     puts '-' * 100
   end
 end
@@ -74,29 +68,77 @@ end
 # EXECUTE
 
 # list = MovieList.new
-# print_films(list.find('Time'), 'Search "Time"')
-# print_films(list.long_films, 'The Longest Films')
-# print_films(list.by_genre('Comedy'), 'Comedy list')
+# print_movies(list.find('Time'), 'Search "Time"')
+# print_movies(list.long_movies, 'The Longest Movies')
+# print_movies(list.by_genre('Comedy'), 'Comedy list')
 # print_directors(list.directors, 'Directors list')
-# print_count_films(list.except_country('USA'), 'Films was made not in USA')
-# print_by_director(list.films, 'Films grouped by director')
-# print_by_months(list.films, 'Films grouped by month')
+# print_count_movies(list.except_country('USA'), 'Movies was made not in USA')
+# print_by_director(list.movies, 'Movies grouped by director')
+# print_by_months(list.movies, 'Movies grouped by month')
 
 
-mylist = MyMovieList.new
+# mylist = MyMovieList.new
+#
+# movies_seen = [
+#     {name: 'The Good, the Bad and the Ugly', myrating: 9},
+#     {name: 'Fight Club', myrating: 10},
+#     {name: 'Goodfellas', myrating: 5},
+#     {name: 'Bicycle Thieves', myrating: 1},
+#     {name: 'Full Metal Jacket', myrating: 2},
+#     {name: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb', myrating: 3},
+# ]
+#
+# mylist.watch_movie('The Godfather', 7, Date.new(2004, 10))
+# mylist.watch_movie('The Dark Knight', 8, Date.new(2003, 3))
+# mylist.watch_movies(movies_seen)
+#
+# print_rec(mylist.recommend_movies, 'ПОСМОТРИТЕ СЕГОДНЯ ВЕЧЕРОМ!')
+# print_rec(mylist.recommend_movies_seen, 'ФИЛЬМЫ, КОТОРЫЕ СТОИТ ПОСМОТРЕТЬ НЕ РАЗ!')
 
-films_seen = [
-    {name: 'The Good, the Bad and the Ugly', myrating: 9},
-    {name: 'Fight Club', myrating: 10},
-    {name: 'Goodfellas', myrating: 5},
-    {name: 'Bicycle Thieves', myrating: 1},
-    {name: 'Full Metal Jacket', myrating: 2},
-    {name: 'Dr. Strangelove or: How I Learned to Stop Worrying and Love the Bomb', myrating: 3},
-]
 
-mylist.watch_film('The Godfather', 7, Date.new(2004, 10))
-mylist.watch_film('The Dark Knight', 8, Date.new(2003, 3))
-mylist.watch_films(films_seen)
 
-print_rec(mylist.recommend_films, 'ПОСМОТРИТЕ СЕГОДНЯ ВЕЧЕРОМ!')
-print_rec(mylist.recommend_films_seen, 'ФИЛЬМЫ, КОТОРЫЕ СТОИТ ПОСМОТРЕТЬ НЕ РАЗ!')
+# LESSON 7
+
+list = MovieList.new
+
+
+# print
+
+# list.print { |movie| "#{movie.year}: #{movie.title}" }
+# list.print
+
+
+# sort by
+
+# movies = list.sorted_by { |movie| [movie.genre, movie.year] }
+# print_movies(movies)
+
+
+# sort by algo
+
+# list.add_sort_algo(:genres_years) { |movie| [movie.genre, movie.year] }
+# movies = list.sorted_by(:genres_years)
+# print_movies(movies)
+
+
+
+# set filters
+
+# list.add_filter(:rating) { |movie, rating| movie.rating > rating }
+# list.add_filter(:genres) do |movie, *genres|
+#   match = false
+#   genres.each { |genre| match = true if movie.genre.include?(genre) }
+#   match
+# end
+# list.add_filter(:years) { |movie, from, to| (from..to).include?(movie.year) }
+
+
+# filter
+
+# movies = list.filter(
+#     rating: 8.2,
+#     genres: ['Comedy', 'Thriller'],
+#     years: [2005, 2010]
+# )
+
+# print_movies(movies)
